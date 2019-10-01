804f26cf543ade9e520d9deb35309315b531c168
Mudit Jain <ciphereck@gmail.com>
2019-10-01 20:45:14 +0530

    Updates

---
+++ precache-manifest.98ed483ba1a1b6ff7c116b3f575aed24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2ce1a0d4d72244afd4c59be62a7ee11",
+    "url": "/confluence-admin-panel/index.html"
+  },
+  {
+    "revision": "f6f083cffaed735b321a",
+    "url": "/confluence-admin-panel/static/css/main.6dff8744.chunk.css"
+  },
+  {
+    "revision": "f3a3479d6566a250a386",
+    "url": "/confluence-admin-panel/static/js/2.47158157.chunk.js"
+  },
+  {
+    "revision": "f6f083cffaed735b321a",
+    "url": "/confluence-admin-panel/static/js/main.c589f5e6.chunk.js"
+  },
+  {
+    "revision": "fea3fe59739ed6184cdc",
+    "url": "/confluence-admin-panel/static/js/runtime~main.dffd56be.js"
+  }
+]);
